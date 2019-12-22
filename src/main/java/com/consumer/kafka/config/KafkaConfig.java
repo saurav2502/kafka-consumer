722778bea9b77a0616e43e7a237acd9f80c0fb43
Saurav Kumar <kumar_sauravsk@outlook.com>
2019-12-22 15:39:59 +0530

    KafkaConfig

--- src/main/java/com/consumer/kafka/config/KafkaConfig.java
+++ src/main/java/com/consumer/kafka/config/KafkaConfig.java
@@ -22,7 +22,7 @@ public class KafkaConfig {
     public ConsumerFactory<String, UserResourse> userConsumerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "GROUP_ID");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "GROUP_JSON");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         return new DefaultKafkaConsumerFactory<String, UserResourse>(config,
@@ -34,6 +34,23 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, UserResourse> containerFactory = new ConcurrentKafkaListenerContainerFactory<>();
         containerFactory.setConsumerFactory(userConsumerFactory());
         return containerFactory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String>getConsumerFactory(){
+        Map<String, Object> configuration = new HashMap<>();
+        configuration.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        configuration.put(ConsumerConfig.GROUP_ID_CONFIG, "GROUP_ID");
+        configuration.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configuration.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configuration);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String>kafkaContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(getConsumerFactory());
+        return factory;
     }
 
 
--- src/main/java/com/consumer/kafka/kafkalistener/KafkaConsumer.java
+++ src/main/java/com/consumer/kafka/kafkalistener/KafkaConsumer.java
@@ -7,9 +7,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "kafka_test", groupId = "GROUP_ID",
-    containerFactory = "kafkaListenerContainerFactory")
+    @KafkaListener(topics = "kafka_test", groupId = "GROUP_JSON",
+          containerFactory = "kafkaListenerContainerFactory")
     public void consume(UserResourse userResourse){
         System.out.println("message Json consumed {}: "+userResourse);
+    }
+
+    @KafkaListener(topics = "test", groupId = "GROUP_ID", containerFactory = "kafkaContainerFactory")
+    public void consumeConsumer(String message){
+        System.out.println("message Json consumed {}: "+message);
     }
 }
